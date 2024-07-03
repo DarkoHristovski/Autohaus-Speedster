b37b802e19b638cefca9d26cc6f67c67661b3ade
Darko Hristovski <hristovski15@gmail.com>
2024-07-03 15:46:43 +0200

    creating a blog posts section

--- wp-content/themes/Autohaus-Speedster/template-parts/block/block-hero.php
+++ wp-content/themes/Autohaus-Speedster/template-parts/block/block-hero.php
@@ -13,7 +13,7 @@ $link = get_sub_field('link');
                         <div class="text">
                         <?php echo $text; ?>
                         </div>
-                        <a class="hero-section-link" target="_blank" href="<?php echo $link['url']; ?>"><?php echo $link['title'] ?></a>
+                        <a class="btn" target="_blank" href="<?php echo $link['url']; ?>"><?php echo $link['title'] ?></a>
                     <?php endif; ?>
                 </div>
             </div>
